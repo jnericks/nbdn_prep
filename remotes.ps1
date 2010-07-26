d24f57bb3e3bd1179e48b0e5d0c457645f0e9e2b
Jean-Paul S. Boodhoo <jp@jpboodhoo.com>
2010-07-26 10:53:55 -0700

    Added the remotes file

---
+++ remotes.ps1
@@ -0,0 +1,4 @@
+("cfarmerga","richlea","rossfuhrman","jaults","karthikg","amolina82","mmarksbury","jzimmerman7","bsnbdn","toddlangdon") | foreach-object
+{
+  git remote add $_ git://github.com/$_/nbdn_prep.git
+}
