e4a17fda4721e3d1a4afa45fd8a4fe9f79cac663
unknown <arielm@.(none)>
2010-07-26 19:22:55 -0400

    did it

--- exercise_to_complete_for_day_1/product/nothinbutdotnetprep/utility/CriteriaFactory.cs
+++ exercise_to_complete_for_day_1/product/nothinbutdotnetprep/utility/CriteriaFactory.cs
@@ -15,5 +15,17 @@ namespace nothinbutdotnetprep.utility
         {
             return new AnonymousCriteria<ItemToFilter>(x => property_accessor(x).Equals(value_to_match));
         }
+
+        public Criteria<ItemToFilter> equal_to_any(PropertyType value_to_match1, PropertyType value_to_match2)
+        {
+            return
+                new OrCriteria<ItemToFilter>(equal_to(value_to_match1), equal_to(value_to_match2));
+        }
+
+        public Criteria<ItemToFilter> not_equal_to(PropertyType value_to_match)
+        {
+
+            return new NotCriteria<ItemToFilter>(this.equal_to(value_to_match));
+        }
     }
 }
