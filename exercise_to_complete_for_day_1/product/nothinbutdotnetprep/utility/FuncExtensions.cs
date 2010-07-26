f69b8935898ebb0e5750aac59ac444f259f5f2e3
mmarksbury <mmarksbury@gmail.com>
2010-07-26 15:59:50 -0700

    added WhereResult

--- exercise_to_complete_for_day_1/product/nothinbutdotnetprep/utility/FuncExtensions.cs
+++ exercise_to_complete_for_day_1/product/nothinbutdotnetprep/utility/FuncExtensions.cs
@@ -4,11 +4,11 @@ namespace nothinbutdotnetprep.utility
 {
     public static class FuncExtensions
     {
-        public static Criteria<ItemToFilter> equal_to<ItemToFilter, PropertyType>(
-            this Func<ItemToFilter, PropertyType> accessor,
-            PropertyType value_to_match)
-        {
-            return new AnonymousCriteria<ItemToFilter>(x => accessor(x).Equals(value_to_match)); 
-        }
+        //public static Criteria<ItemToFilter> equal_to<ItemToFilter, PropertyType>(
+        //    this Func<ItemToFilter, PropertyType> accessor,
+        //    PropertyType value_to_match)
+        //{
+        //    return new AnonymousCriteria<ItemToFilter>(x => accessor(x).Equals(value_to_match));
+        //}
     }
 }
--- exercise_to_complete_for_day_1/product/nothinbutdotnetprep/utility/Where.cs
+++ exercise_to_complete_for_day_1/product/nothinbutdotnetprep/utility/Where.cs
@@ -4,9 +4,27 @@ namespace nothinbutdotnetprep.utility
 {
     public class Where<ItemToFilter>
     {
-        public static Func<ItemToFilter,ReturnType> has_a<ReturnType>(Func<ItemToFilter, ReturnType> property_accessor)
+        public static WhereResult<ItemToFilter, PropertyType> has_a<PropertyType>(Func<ItemToFilter, PropertyType> property_accessor)
         {
-            return property_accessor;
+            return new WhereResult<ItemToFilter, PropertyType>(property_accessor);
+        }
+
+    }
+
+
+    public class WhereResult<ItemToFilter, PropertyType>
+    {
+        private Func<ItemToFilter, PropertyType> property_accessor;
+
+        public WhereResult(Func<ItemToFilter, PropertyType> property_expression)
+        {
+            this.property_accessor = property_expression;
+        }
+
+        public Criteria<ItemToFilter> equal_to(PropertyType value_to_match)
+        {
+            return new AnonymousCriteria<ItemToFilter>(x => property_accessor(x).Equals(value_to_match));
         }
     }
+    
 }
