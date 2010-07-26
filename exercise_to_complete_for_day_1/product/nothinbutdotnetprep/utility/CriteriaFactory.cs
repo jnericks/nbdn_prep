7c11a748015683967af8d855abbf839733cc02d5
Jean-Paul S. Boodhoo <jp@jpboodhoo.com>
2010-07-26 19:07:06 -0400

    Continuing the Where class

---
+++ exercise_to_complete_for_day_1/product/nothinbutdotnetprep/utility/CriteriaFactory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace nothinbutdotnetprep.utility
+{
+    public class CriteriaFactory<ItemToFilter, PropertyType>
+    {
+        private Func<ItemToFilter, PropertyType> property_accessor;
+
+        public CriteriaFactory(Func<ItemToFilter, PropertyType> property_expression)
+        {
+            this.property_accessor = property_expression;
+        }
+
+        public Criteria<ItemToFilter> equal_to(PropertyType value_to_match)
+        {
+            return new AnonymousCriteria<ItemToFilter>(x => property_accessor(x).Equals(value_to_match));
+        }
+    }
+}
--- exercise_to_complete_for_day_1/product/nothinbutdotnetprep/utility/Where.cs
+++ exercise_to_complete_for_day_1/product/nothinbutdotnetprep/utility/Where.cs
@@ -4,27 +4,10 @@ namespace nothinbutdotnetprep.utility
 {
     public class Where<ItemToFilter>
     {
-        public static WhereResult<ItemToFilter, PropertyType> has_a<PropertyType>(Func<ItemToFilter, PropertyType> property_accessor)
+        public static CriteriaFactory<ItemToFilter, PropertyType> has_a<PropertyType>(Func<ItemToFilter, PropertyType> property_accessor)
         {
-            return new WhereResult<ItemToFilter, PropertyType>(property_accessor);
+            return new CriteriaFactory<ItemToFilter, PropertyType>(property_accessor);
         }
 
     }
-
-
-    public class WhereResult<ItemToFilter, PropertyType>
-    {
-        private Func<ItemToFilter, PropertyType> property_accessor;
-
-        public WhereResult(Func<ItemToFilter, PropertyType> property_expression)
-        {
-            this.property_accessor = property_expression;
-        }
-
-        public Criteria<ItemToFilter> equal_to(PropertyType value_to_match)
-        {
-            return new AnonymousCriteria<ItemToFilter>(x => property_accessor(x).Equals(value_to_match));
-        }
-    }
-    
 }
