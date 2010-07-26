14ba0d9dc0f649f0d86895b0d83b650a12607970
Jean-Paul S. Boodhoo <jp@jpboodhoo.com>
2010-07-26 14:07:10 -0400

    Updating the remotes file

--- remotes.ps1
+++ remotes.ps1
@@ -1,4 +1,3 @@
-("cfarmerga","richlea","rossfuhrman","jaults","karthikg","amolina82","mmarksbury","jzimmerman7","bsnbdn","toddlangdon") | foreach-object
-{
-  git remote add $_ git://github.com/$_/nbdn_prep.git
+("cfarmerga","richlea","rossfuhrman","jaults","karthikg","amolina82","mmarksbury","jzimmerman7","bsnbdn","toddlangdon") | foreach-object{
+  git remote add $_ "git://github.com/$_/nbdn_prep.git"
 }
