c695dfcd73578174a4f8dfa548fb8899fe8aa3cb
Jean-Paul S. Boodhoo <jp@jpboodhoo.com>
2010-07-27 00:02:37 -0400

    Updated the searching exercise

--- exercise_to_complete_for_day_1/product/nothinbutdotnetprep/utility/filtering/CriteriaFactory.cs
+++ exercise_to_complete_for_day_1/product/nothinbutdotnetprep/utility/filtering/CriteriaFactory.cs
@@ -2,7 +2,6 @@
 {
     public interface CriteriaFactory<ItemToFilter, PropertyType>
     {
-        CriteriaFactory<ItemToFilter, PropertyType> not { get; }
         Criteria<ItemToFilter> equal_to(PropertyType value_to_match);
         Criteria<ItemToFilter> equal_to_any(params PropertyType[] values);
     }
---
+++ exercise_to_complete_for_day_1/product/nothinbutdotnetprep/utility/sorting/ComparerExtensions.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+
+namespace nothinbutdotnetprep.utility.sorting
+{
+    public static class ComparerExtensions
+    {
+        public static IComparer<T> followed_by<T>(this IComparer<T> first, IComparer<T> second)
+        {
+            return new ChainedComparer<T>(first, second);
+        }
+    }
+}
