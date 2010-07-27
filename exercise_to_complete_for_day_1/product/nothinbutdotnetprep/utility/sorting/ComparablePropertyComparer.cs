57207bd8bbcc14b16a1a4f6a69e3e75f53a542e3
Jean-Paul S. Boodhoo <jp@jpboodhoo.com>
2010-07-27 12:59:56 -0400

    Updated the sort

---
+++ exercise_to_complete_for_day_1/product/nothinbutdotnetprep/utility/sorting/ComparablePropertyComparer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace nothinbutdotnetprep.utility.sorting
+{
+    public class ComparablePropertyComparer<ItemToSort,PropertyType> : IComparer<ItemToSort> where PropertyType : IComparable<PropertyType>
+    {
+        Func<ItemToSort, PropertyType> property_accessor;
+
+        public ComparablePropertyComparer(Func<ItemToSort, PropertyType> property_accessor)
+        {
+            this.property_accessor = property_accessor;
+        }
+
+        public int Compare(ItemToSort x, ItemToSort y)
+        {
+            return property_accessor(x).CompareTo(property_accessor(y));
+        }
+    }
+}
--- exercise_to_complete_for_day_1/product/nothinbutdotnetprep/utility/sorting/Compare.cs
+++ exercise_to_complete_for_day_1/product/nothinbutdotnetprep/utility/sorting/Compare.cs
@@ -1,29 +1,14 @@
 using System;
 using System.Collections.Generic;
-using nothinbutdotnetprep.collections;
 
 namespace nothinbutdotnetprep.utility.sorting
 {
     public class Compare<ItemToSort>
     {
-        public static IComparer<ItemToSort> by_descending<PropertyType>(Func<ItemToSort, PropertyType> func) where PropertyType : IComparable<PropertyType>
+        public static IComparer<ItemToSort> by_descending<PropertyType>(Func<ItemToSort, PropertyType> property_accessor)
+            where PropertyType : IComparable<PropertyType>
         {
-            return new DescendingPropertyComparer<ItemToSort, PropertyType>(func);
-        }
-    }
-
-    public class DescendingPropertyComparer<ItemToSort, PropertyType> : IComparer<ItemToSort> where PropertyType : IComparable<PropertyType>
-    {
-        private readonly Func<ItemToSort, PropertyType> _func;
-
-        public DescendingPropertyComparer(Func<ItemToSort, PropertyType> func)
-        {
-            _func = func;
-        }
-
-        public int Compare(ItemToSort x, ItemToSort y)
-        {
-            return -_func(x).CompareTo(_func(y));
+            return new ReverseComparer<ItemToSort>(new ComparablePropertyComparer<ItemToSort,PropertyType>(property_accessor)); 
         }
     }
 }
